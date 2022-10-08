6d24787b8741f2ad8932af8e03c36b2c53be6cf3
edskorn <ed.skorn@gmail.com>
2022-10-08 21:22:56 +0700

    added method calcAction()

--- Main.java
+++ Main.java
@@ -2,46 +2,45 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 public class Main {
+    public static final String errFormat = "Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *), разделенные одинарными пробелами";
+    public static final String errRange = "Калькулятор принимает на вход числа от 1 до 10 включительно и от I до X";
+    public static final String errRomanNegative = "В римской системе нет отрицательных чисел и нуля";
+    public static final String errDifferentNumberSystem = "Используются одновременно разные системы счисления";
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // убрать бесконечный цикл при отправке на проверку
         while (true) {
-            System.out.print("Enter your expression: ");
-            String s = br.readLine();
-            if (s.equals("exit")) break;
+            System.out.print("Введите выражение: ");
+            String inputString = br.readLine();
+            if (inputString.equals("exit")) break;
             try {
-                String sss = calc(s);
-                System.out.println("Hello calc! " + sss);
+                System.out.println(calc(inputString));
             } catch (IOException e) {
-                System.out.println("Что-то пошло не так. Сорян, бро.");
                 System.out.println(e.getMessage());
             }
         }
     }
     public static String calc(String input) throws IOException
     {
-        // заменить на константы
-        String errFormat = "Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *), разделенные одинарными пробелами";
-        String errRange = "Калькулятор принимает на вход числа от 1 до 10 включительно и от I до X";
-        String errRomanNegative = "В римской системе нет отрицательных чисел и нуля";
-        String errDifferentNumberSystem = "Используются одновременно разные системы счисления";
         RomeDigit aRome;
         RomeDigit bRome;
         int aArab;
         int bArab;
-
         String[] split = input.split(" ");
+
+        // Проверяем что на входе - 3 блока или нет (2 операнда + 1 оператор)
         if (split.length!=3) throw new IOException(errFormat);
 
         try {
-            // Парсим первый операнд - если римский, то продолжаем, если нет идём в catch проверить арабский ли.
+            // Парсим первый операнд - если римский, то продолжаем, если нет идём в catch проверить является ли арабским.
             aRome = RomeDigit.valueOf(split[0]);
 
-            //Парсим второй операнд
+            //Парсим второй операнд в римское число
             try {
                 bRome = RomeDigit.valueOf(split[2]);
             } catch (IllegalArgumentException e) {
                 try {
+                    // Проверим не является ли второй операнд числом из другой системы счисления
                     Integer.parseInt(split[2]);
                     throw new IOException(errDifferentNumberSystem);
                 }
@@ -50,42 +49,26 @@ public class Main {
                 }
             }
 
-            //Проводим вычисление, если оператор допустимый
-            int result;
-            switch (split[1]) {
-                case "+" -> result = aRome.getArabicNumber() + bRome.getArabicNumber();
-                case "-" -> {
-                    if (aRome.getArabicNumber() <= bRome.getArabicNumber()) {
-                        throw new IOException(errRomanNegative);
-                    }
-                    result = aRome.getArabicNumber() - bRome.getArabicNumber();
-                }
-                case "*" -> result = aRome.getArabicNumber() * bRome.getArabicNumber();
-                case "/" -> {
-                    result = aRome.getArabicNumber() / bRome.getArabicNumber();
-                    if (result<1) {
-                        throw new IOException(errRomanNegative);
-                    }
-                }
-                default ->
-                        throw new IOException(errFormat);
-            }
+            //Проводим вычисление
+            int result = calcAction(aRome.getArabicNumber(), bRome.getArabicNumber(), split[1], true);
 
             //Выводим результат римскими цифрами
             return "Input: " + aRome + split[1] + bRome + " Result: " + RomanNumber.toRoman(result);
 
         } catch (IllegalArgumentException e) {
             try {
+                // Парсим первый операнд в арабское число
                 aArab = Integer.parseInt(split[0]);
                 if (aArab<1 || aArab>10) throw new IOException(errRange);
 
-                //Парсим второй операнд
+                //Парсим второй операнд в арабское число
                 try {
                     bArab = Integer.parseInt(split[2]);
                     if (bArab<1 || bArab>10) throw new IOException(errRange);
                 }
                 catch (NumberFormatException ee) {
                     try {
+                        // Проверим не является ли второй операнд числом из другой системы счисления
                         RomeDigit.valueOf(split[2]);
                         throw new IOException(errDifferentNumberSystem);
                     } catch (IllegalArgumentException eee) {
@@ -93,14 +76,8 @@ public class Main {
                     }
                 }
 
-                int result;
-                switch (split[1]) {
-                    case "+" -> result = aArab + bArab;
-                    case "-" -> result = aArab - bArab;
-                    case "*" -> result = aArab * bArab;
-                    case "/" -> result = aArab / bArab;
-                    default -> throw new IOException(errFormat);
-                }
+                //Проводим вычисление
+                int result = calcAction(aArab, bArab, split[1], false);
 
                 //Выводим результат арабскими цифрами
                 return "Input: " + aArab + split[1] + bArab + " Result: " + result;
@@ -109,5 +86,20 @@ public class Main {
                 throw new IOException(errFormat);
             }
         }
+    }
+
+    public static int calcAction(int a, int b, String operator, boolean roman) throws IOException
+    {
+        int result;
+        switch (operator) {
+            case "+" -> result = a + b;
+            case "-" -> result = a - b;
+            case "*" -> result = a * b;
+            case "/" -> result = a / b;
+            default -> throw new IOException(errFormat);
+        }
+        if (roman && result<1) throw new IOException(errRomanNegative);
+
+        return result;
     }
 }
--- RomeDigit.java
+++ RomeDigit.java
@@ -1,5 +1,4 @@
 public enum RomeDigit {
-    // упростить через index
     I (1), II(2), III(3), IV(4), V(5),
     VI(6), VII(7), VIII(8), IX(9), X(10);
 
