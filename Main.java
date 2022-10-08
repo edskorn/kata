74d89b191818650a481ccd254005f06a7b87c379
edskorn <ed.skorn@gmail.com>
2022-10-08 16:03:24 +0700

    working version with exceptions

--- Main.java
+++ Main.java
@@ -4,49 +4,128 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter your expression: ");
-        String s = br.readLine();
-//        System.out.print("Enter Integer: ");
-//        try {
-//            int i = Integer.parseInt(br.readLine());
-//        } catch(NumberFormatException nfe) {
-//            System.err.println("Invalid Format!");
-//        }
-        String sss = calc(s);
-        System.out.println("Hello calc! " + sss);
+        // убрать бесконечный цикл при отправке на проверку
+        while (true) {
+            System.out.print("Enter your expression: ");
+            String s = br.readLine();
+            if (s.equals("exit")) break;
+            try {
+                String sss = calc(s);
+                System.out.println("Hello calc! " + sss);
+            } catch (IOException e) {
+                System.out.println("Что-то пошло не так. Сорян, бро.");
+                System.out.println(e.getMessage());
+            }
+        }
     }
-    public static String calc(String input)
+    public static String calc(String input) throws IOException
     {
-         // boolean rome = false;
+        // заменить на константы
+        String errFormat = "Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *), разделенные одним пробелом";
+        String errRange = "Калькулятор принимает на вход числа от 1 до 10 включительно и от I до X";
+        String errRomanNegative = "В римской системе нет отрицательных чисел и нуля";
+        String errDifferentNumberSystem = "Используются одновременно разные системы счисления";
+        RomeDigit aRome;
+        RomeDigit bRome;
+        int aArab;
+        int bArab;
+
+        if (!input.contains(" ") || input.indexOf(" ")==0) throw new IOException(errFormat);
+
         String first = input.substring(0, input.indexOf(" "));
-        RomeDigit aRome = RomeDigit.valueOf(first);
-//        byte a = Byte.parseByte(first);
-        String operator = input.substring(first.length()+1, input.indexOf(" ", first.length()+1));
-        String second = input.substring(first.length()+operator.length()+2);
-        RomeDigit bRome = RomeDigit.valueOf(second);
-//        byte b = Byte.parseByte(second);
-        int result = 1;
-        switch (operator) {
-            case "+":
-                result = aRome.getArabicNumber() + bRome.getArabicNumber();
-                break;
-            case "-":
-                if (aRome.getArabicNumber() <= bRome.getArabicNumber()) {
-                    System.out.println("throws Exception //т.к. в римской системе нет отрицательных чисел и нуля");
-                } else {
+        try {
+            // Парсим первый операнд - если римский, то продолжаем, если нет идём в catch проверить арабский ли.
+            aRome = RomeDigit.valueOf(first);
+
+            // Парсим оператор
+            if ((input.indexOf(" ", first.length()+1)==-1) || (input.indexOf(" ", first.length()+1)==first.length()+1)) {
+                throw new IOException(errFormat);
+            }
+            String operator = input.substring(first.length()+1, input.indexOf(" ", first.length()+1));
+            // Проверяем, не заканчивается ли выражение на операторе
+            if (input.length()-first.length()-operator.length()-2==0) throw new IOException(errFormat);
+
+            //Парсим второй операнд
+            String second = input.substring(first.length()+operator.length()+2);
+            try {
+                bRome = RomeDigit.valueOf(second);
+            } catch (IllegalArgumentException e) {
+                try {
+                    Integer.parseInt(second);
+                    throw new IOException(errDifferentNumberSystem);
+                }
+                catch (NumberFormatException ee) {
+                    throw new IOException(errRange);
+                }
+            }
+
+            //Проводим вычисление, если оператор допустимый
+            int result;
+            switch (operator) {
+                case "+" -> result = aRome.getArabicNumber() + bRome.getArabicNumber();
+                case "-" -> {
+                    if (aRome.getArabicNumber() <= bRome.getArabicNumber()) {
+                        throw new IOException(errRomanNegative);
+                    }
                     result = aRome.getArabicNumber() - bRome.getArabicNumber();
                 }
-                break;
-            case "*":
-                result = aRome.getArabicNumber() * bRome.getArabicNumber();
-                break;
-            case "/":
-                result = aRome.getArabicNumber() / bRome.getArabicNumber();
-                break;
-            default:
-                System.out.println("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+                case "*" -> result = aRome.getArabicNumber() * bRome.getArabicNumber();
+                case "/" -> {
+                    result = aRome.getArabicNumber() / bRome.getArabicNumber();
+                    if (result<1) {
+                        throw new IOException(errRomanNegative);
+                    }
+                }
+                default ->
+                        throw new IOException(errFormat);
+            }
+
+            //Выводим результат римскими цифрами
+            return "Input: " + aRome + operator + bRome + " Result: " + RomanNumber.toRoman(result);
+
+        } catch (IllegalArgumentException e) {
+            try {
+                aArab = Integer.parseInt(first);
+                if (aArab<1 || aArab>10) throw new IOException(errRange);
+
+                // Парсим оператор
+                if ((input.indexOf(" ", first.length()+1)==-1) || (input.indexOf(" ", first.length()+1)==first.length()+1)) {
+                    throw new IOException(errFormat);
+                }
+                String operator = input.substring(first.length()+1, input.indexOf(" ", first.length()+1));
+                // Проверяем, не заканчивается ли выражение на операторе
+                if (input.length()-first.length()-operator.length()-2==0) throw new IOException(errFormat);
+
+                //Парсим второй операнд
+                String second = input.substring(first.length()+operator.length()+2);
+                try {
+                    bArab = Integer.parseInt(second);
+                    if (bArab<1 || bArab>10) throw new IOException(errRange);
+                }
+                catch (NumberFormatException ee) {
+                    try {
+                        RomeDigit.valueOf(second);
+                        throw new IOException(errDifferentNumberSystem);
+                    } catch (IllegalArgumentException eee) {
+                        throw new IOException(errRange);
+                    }
+                }
+
+                int result;
+                switch (operator) {
+                    case "+" -> result = aArab + bArab;
+                    case "-" -> result = aArab - bArab;
+                    case "*" -> result = aArab * bArab;
+                    case "/" -> result = aArab / bArab;
+                    default -> throw new IOException(errFormat);
+                }
+
+                //Выводим результат арабскими цифрами
+                return "Input: " + aArab + operator + bArab + " Result: " + result;
+            }
+            catch (NumberFormatException ee) {
+                throw new IOException(errFormat);
+            }
         }
-//      return "Input: " + a + operator + b + " Result: " + result;
-        return "Input: " + aRome + operator + bRome + " Result: " + RomanNumber.toRoman(result);
     }
 }
--- RomeDigit.java
+++ RomeDigit.java
@@ -1,4 +1,5 @@
 public enum RomeDigit {
+    // упростить через index
     I (1), II(2), III(3), IV(4), V(5),
     VI(6), VII(7), VIII(8), IX(9), X(10);
 
