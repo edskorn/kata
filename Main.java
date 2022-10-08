e8928c638a509d55f36fd0c11617af4f9497af49
edskorn <ed.skorn@gmail.com>
2022-10-08 12:24:23 +0700

    calc roman numbers without exceptions

--- Main.java
+++ Main.java
@@ -6,12 +6,12 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter your expression: ");
         String s = br.readLine();
-        /*System.out.print("Enter Integer: ");
-        try {
-            int i = Integer.parseInt(br.readLine());
-        } catch(NumberFormatException nfe) {
-            System.err.println("Invalid Format!");
-        }*/
+//        System.out.print("Enter Integer: ");
+//        try {
+//            int i = Integer.parseInt(br.readLine());
+//        } catch(NumberFormatException nfe) {
+//            System.err.println("Invalid Format!");
+//        }
         String sss = calc(s);
         System.out.println("Hello calc! " + sss);
     }
@@ -19,11 +19,34 @@ public class Main {
     {
          // boolean rome = false;
         String first = input.substring(0, input.indexOf(" "));
-        byte a = Byte.parseByte(first);
+        RomeDigit aRome = RomeDigit.valueOf(first);
+//        byte a = Byte.parseByte(first);
         String operator = input.substring(first.length()+1, input.indexOf(" ", first.length()+1));
         String second = input.substring(first.length()+operator.length()+2);
-        byte b = Byte.parseByte(second);
-        int result = a+b;
-        return "Input: " + a + operator + b + " Result: " + result;
+        RomeDigit bRome = RomeDigit.valueOf(second);
+//        byte b = Byte.parseByte(second);
+        int result = 1;
+        switch (operator) {
+            case "+":
+                result = aRome.getArabicNumber() + bRome.getArabicNumber();
+                break;
+            case "-":
+                if (aRome.getArabicNumber() <= bRome.getArabicNumber()) {
+                    System.out.println("throws Exception //т.к. в римской системе нет отрицательных чисел и нуля");
+                } else {
+                    result = aRome.getArabicNumber() - bRome.getArabicNumber();
+                }
+                break;
+            case "*":
+                result = aRome.getArabicNumber() * bRome.getArabicNumber();
+                break;
+            case "/":
+                result = aRome.getArabicNumber() / bRome.getArabicNumber();
+                break;
+            default:
+                System.out.println("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+        }
+//      return "Input: " + a + operator + b + " Result: " + result;
+        return "Input: " + aRome + operator + bRome + " Result: " + RomanNumber.toRoman(result);
     }
 }
---
+++ RomeDigit.java
@@ -0,0 +1,14 @@
+public enum RomeDigit {
+    I (1), II(2), III(3), IV(4), V(5),
+    VI(6), VII(7), VIII(8), IX(9), X(10);
+
+    private int arabicNumber;
+
+    RomeDigit (int arabicNumber){
+        this.arabicNumber = arabicNumber;
+    }
+
+    public int getArabicNumber() {
+        return arabicNumber;
+    }
+}
